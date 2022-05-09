b1ad063282972883041f117974361c7ac2d22cdb
madhuri7patel <madhuri7patel@gmail.com>
2022-05-09 10:48:25 +0530

    landingpage

---
+++ components/footer.js
@@ -0,0 +1,233 @@
+function footer() {
+  return `
+  <div id="footer1">
+        <div>
+            <h2 class="footerSubscribe_label"> Newsletter </h2>
+            <input type="email" name="email" class="inputmail" placeholder="Enter Email to get 50 Points"
+                autocomplete="off">
+            <button type="submit" class="submit">Subscribe</button>
+
+        </div>
+
+        <div class="footerSoical">
+            <a href="https://www.facebook.com/Gearbestshopping" class="footerSoical_item iconBg_icon-fb" rel="nofollow">
+                <i class="fa-brands fa-facebook"></i> </a>
+
+            <a href="https://goo.gl/qXtLAh" class="footerSoical_item iconBg_icon-msg" rel="nofollow">
+                <i class="fa-brands fa-facebook-messenger"></i> </a>
+
+            <a href="https://www.youtube.com/user/TheGearBest" class="footerSoical_item iconBg_icon-youtube"
+                rel="nofollow"> <i class="fa-brands fa-youtube"></i> </a>
+
+            <a href="https://www.pinterest.com/gearbestlife/" class="footerSoical_item iconBg_icon-pinterest"
+                rel="nofollow"> <i class="fa-brands fa-pinterest"></i> </a>
+
+            <a href="https://vk.com/gearbest" class="footerSoical_item iconBg_icon-vk" rel="nofollow">
+                <i class="fa-brands fa-vk"></i> </a>
+
+            <a href="https://www.instagram.com/gearbest/" class="footerSoical_item iconBg_icon-instagram"
+                rel="nofollow"> <i class="fa-brands fa-instagram-square"></i> </a>
+
+            <a href="https://twitter.com/TheGearbest/" class="footerSoical_item iconBg_icon-twitter" rel="nofollow">
+                <i class="fa-brands fa-twitter-square"></i> </a>
+
+            <a href="https://www.gearbest.com/blog/" class="footerSoical_item iconBg_icon-blog">
+                <i class="fa-brands fa-blogger"></i> </a>
+
+            <a href="http://bit.ly/2LadoyE" class="footerSoical_item iconBg_icon-tumblr" rel="nofollow">
+                <i class="fa-brands fa-tumblr"></i> </a>
+
+        </div>
+    </div>
+
+    <div class="footerLinks footerCenter">
+        <dl class="footerLinks_col">
+            <dt class="footerLinks_colTitle">Company Information</dt>
+            <dd> <a href="https://www.gearbest.com/about/about-us.html" class="footerLinks_colLink" rel="nofollow">
+                    About Us
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/top-searches/" class="footerLinks_colLink" rel="nofollow">
+                    Top Searches
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/privacy-policy.html" class="footerLinks_colLink"
+                    rel="nofollow">
+                    Privacy Policy
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/terms-and-conditions.html" class="footerLinks_colLink"
+                    rel="nofollow">
+                    Terms and Conditions
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/Intellectual-Property-Policy.html" class="footerLinks_colLink"
+                    rel="nofollow">
+                    Intellectual Property Policy
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/testimonials.html" class="footerLinks_colLink" rel="nofollow">
+                    Testimonials
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/contactus.html" class="footerLinks_colLink" rel="nofollow">
+                    Contact Us
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/report-security-issue.html" class="footerLinks_colLink"
+                    rel="nofollow">
+                    Report Security Issue
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/suppliers" class="footerLinks_colLink" rel="nofollow">
+                    Sell on Gearbest
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/cookie-policy.html" class="footerLinks_colLink" rel="nofollow">
+                    Cookies Policy
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/top-brands.html" class="footerLinks_colLink" rel="nofollow">
+                    Top Brands
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/blog/" class="footerLinks_colLink">
+                    Gearbest Official Blog
+                </a> </dd>
+        </dl>
+        <dl class="footerLinks_col">
+            <dt class="footerLinks_colTitle">Customer Service</dt>
+            <dd> <a href="https://www.gearbest.com/about/shipping-methods.html" class="footerLinks_colLink"
+                    rel="nofollow">
+                    Shipping Information
+                </a> </dd>
+            <dd> <a href="https://user.gearbest.com/index#/favor/goods" class="footerLinks_colLink" rel="nofollow">
+                    My Favorites
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/faq-for-newsletter-subscription.html"
+                    class="footerLinks_colLink" rel="nofollow">
+                    FAQ For Newsletter Subscription
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/about-points.html" class="footerLinks_colLink" rel="nofollow">
+                    My Points
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/warranty-and-return.html" class="footerLinks_colLink"
+                    rel="nofollow">
+                    Warranty and Return
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/payment-methods.html" class="footerLinks_colLink"
+                    rel="nofollow">
+                    Payment Methods
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/deposit.html" class="footerLinks_colLink" rel="nofollow">
+                    Deposit Expansion
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/faq.html" class="footerLinks_colLink" rel="nofollow">
+                    FAQ &amp; Support
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/sitemap.html" class="footerLinks_colLink">
+                    Sitemap
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/gb-wallet.html" class="footerLinks_colLink" rel="nofollow">
+                    Gearbest Wallet Instruction
+                </a> </dd>
+            <dd> <a href="https://www.gearbest.com/about/faqs-about-covid-19.html" class="footerLinks_colLink"
+                    rel="nofollow">
+                    FAQs about COVID-19
+                </a> </dd>
+        </dl>
+        <dl class="footerLinks_col">
+            <dt class="footerLinks_colTitle">Other Business</dt>
+            <dd> <a href="https://www.gearbest.com/about/make-money.html" class="footerLinks_colLink" rel="nofollow">
+                    Partnership Programs
+                </a> </dd>
+            <dd> <a href="https://affiliate.gearbest.com/" class="footerLinks_colLink">
+                    Associate Program
+                </a> </dd>
+        </dl>
+        <div class="footerApprecom" rel="nofollow">
+            <div class="appDownload_panel">
+                <div class="appDownload_title">Download App!</div>
+                <p class="appDownload_desc" title="Save $3 with App &amp; New User Only">Save $3 with App &amp; New User
+                    Only</p>
+                <div class="appDownload_area"> <a href="https://gearbest.app.link/pctopios"
+                        class="appDownload_appStore"></a> <a href="https://gearbest.app.link/pctopandroid"
+                        class="appDownload_googlePlay"></a> </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footWebauth footerCenter"> <a href="https://www.gearbest.com/about/warranty-and-return.html"
+            class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/money.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/money.png"
+                data-was-processed="true"> </a> <a href="https://www.mcafeesecure.com/verify?host=gearbest.com"
+            class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/m.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/m.png" data-was-processed="true">
+        </a> <a href="https://www.resellerratings.com/store/gearbest" class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded" src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/rese.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/rese.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/paypal.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/paypal.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded" src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/visa.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/visa.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded" src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/a.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/a.png" data-was-processed="true">
+        </a> <a class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/ww.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/ww.png" data-was-processed="true">
+        </a> <a class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/jcb.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/jcb.png" data-was-processed="true">
+        </a> <a class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/dic.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/dic.png" data-was-processed="true">
+        </a> <a class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/ya.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/ya.png" data-was-processed="true">
+        </a> <a class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/d.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/d.png" data-was-processed="true">
+        </a> <a class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/qiwi.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/qiwi.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded" src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/oxxo.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/oxxo.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/webmoney.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/webmoney.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded" src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/bo.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/bo.png" data-was-processed="true">
+        </a> <a class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/postepay.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/postepay.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/giropay.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/giropay.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/sofort.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/sofort.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded" src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/eps.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/eps.png" data-was-processed="true">
+        </a> <a class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/p.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/p.png" data-was-processed="true">
+        </a> <a class="footWebauth_item" rel="nofollow"> <img class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/pago.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/banner/20180407_2974/pago.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/others/20190222_7819/Visa-Logo.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/others/20190222_7819/Visa-Logo.png"
+                data-was-processed="true"> </a> <a class="footWebauth_item" rel="nofollow"> <img
+                class="js-lazyload lazy-loaded"
+                src="https://uidesign.gbtcdn.com/GB/image/others/20190222_7819/m-Logo.png"
+                data-lazy="https://uidesign.gbtcdn.com/GB/image/others/20190222_7819/m-Logo.png"
+                data-was-processed="true"> </a> </div>
+
+    <p class="footCopyright">
+        Copyright © 2014-2021 Gearbest.com. All Rights Reserved.
+    </p>`;
+}
+
+export { footer };
---
+++ product.js
@@ -0,0 +1,259 @@
+import { footer } from "./components/footer.js";
+document.getElementById("footer").innerHTML = footer();
+var image = [
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16454/goods_img-v1/42405dc55cb8.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/store/6615272824267153408/16421/goods_img-v4/a0da751fd8f8.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16416/goods_img-v1/5e4723996cff.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16499/goods_img-v5/6233d35f6f8a.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6878030535709618176/16492/goods_img-v1/bb1748b1d9f2.jpg",
+    title:
+      " Xiaomi Mi Water Immersing Sensor Smart Wireless Water Leak Detector IP67 Dustproof Waterproof Works With Mijia APP - White",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16414/goods_img-v1/5f26bc001550.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16433/goods_img-v1/0b0ff330204b.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/store/6878025592978206720/16420/goods_img-v3/d5477f83e8f5.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/store/6878399324569661440/16425/goods_img-v3/7a007e142207.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6900746726869757952/16492/goods_img-v3/6d537d29b48f.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16456/goods_img-v1/5ef9d5b1a9a0.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6900746726869757952/16496/goods_img-v5/64df3bb8819d.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6896502277952172032/16460/goods_img-v1/524849cb314c.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6878024886355423232/16417/goods_img-v1/0b74ebf9ee1a.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6882688590640508928/16448/goods_img-v3/6f9c892c7ec6.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6866325577285169152/16409/goods_img-v1/5b7959d3089f.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6900746726869757952/16492/goods_img-v3/e0b65bad2e24.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6866325577285169152/16414/goods_img-v1/76cb26d2019a.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16499/goods_img-v5/3aef13f28d2e.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6878025592978206720/16484/goods_img-v1/068099f7545f.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6880475736935952384/16427/goods_img-v1/1993a3019ba8.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/16152/goods_img-v2/78e3dccea62c.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/15928/goods_img-v3/591815f358a7.jpg",
+
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6866331803104505856/16396/goods_img-v1/526c71907b5c.jpg",
+    image_url1:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16414/goods_img-v1/5f26bc001550.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/store/6866331803104505856/16499/goods_img-v5/cc85a2308636.jpg",
+
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6882688590640508928/16500/goods_img-v1/810d59db032c.jpg",
+
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6866325577285169152/16409/goods_img-v1/ee708b0fcc32.jpg",
+
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/15928/goods_img-v3/591815f358a7.jpg",
+
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6878024886355423232/16417/goods_img-v1/0b74ebf9ee1a.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/store/6866331803104505856/16499/goods_img-v5/cc85a2308636.jpg",
+
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16414/goods_img-v1/5f26bc001550.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+  {
+    image_url:
+      "https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16454/goods_img-v1/42405dc55cb8.jpg",
+    title:
+      "OUKITEL WP15 5G Global Bands 15600mAh Battery Dimensity 700 8GB 128GB 6.52 inch 48MP Triple Camera NFC IP68&IP69K Waterproof Rugged Smartphone - Black Hong Kong",
+    price: "$295.00",
+  },
+];
+
+var arr = JSON.parse(localStorage.getItem("cart")) || [];
+image.map(function (ele) {
+  var box = document.createElement("div");
+
+  var img = document.createElement("img");
+  img.src = ele.image_url;
+  //   img.addEventListener("click");
+  var title = document.createElement("h5");
+  title.innerText = ele.title;
+
+  var price = document.createElement("h2");
+  price.innerText = ele.price;
+  price.setAttribute("id", "pricee");
+
+  box.append(img, title, price);
+  document.querySelector("#container").append(box);
+
+  arr.push(ele);
+  localStorage.setItem("image", JSON.stringify(arr));
+});
